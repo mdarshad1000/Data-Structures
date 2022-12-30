51aa0193e3df28a5531ada819f9b61d331922a44
Mohammad Arshad <mdarshad1000@gmail.com>
2022-12-30 06:48:24 +0530

    Singly Linked List

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -1,12 +1,99 @@
 """
  1. Linked is a linear data structure.
  2. Made chain of nodes.
- 3. Node - Element of LinkedList
+ 3. Node - Element of LinkedList.
  4. Each node contains data and link of the next node.
- 5. Last node stores data and reference to empty value or Null
- 6. Reference(link) of node `n` is stored in node `n-1`
+ 5. Last node stores data and reference to empty value or Null.
+ 6. Reference(link) of node `n` is stored in node `n-1`.
  7. First node is called head/front.
  8. First node is called tail/end.
- 8. Reference of first node is 
+ 9. Dynamic data structure: Size need not to be mentioned.
+ 10. Node/elements are stored randomly in memory.
+ 11. We don't need Contagious Memory Location - allocating consecutive memory blocks.
+ 12. Advantage: - Uses Dyanmic memory location.
+                - Insertion and Deletion is easier.
+                - Can be used to implement Stack, Graph, Queue.
+                - Represent and manipulate polynomials.
+ 13. Disadvantage: - Needs extra memory
+                   - Randomly accessing elements is not possible.
+                    (Since each nodes know the reference of the next node)
 """
+
+# Types of Linked List
+"""
+ 1. Singly Linked List
+ 2. Doubly Linked List
+ 3. Circular Linked List
+"""
+
+# SINGLY LINKED LIST
+"""
+  - If each nodes contain only one link i.e link of the next node.
+  - Every node contains data and link/reference to the next node.
+  - Last node contains referenc to Null/empty value.
+  - First node is Head
+  - Allows you to move in forward direction.
+  - Backward direction movement is not easier.
+  - Operations: Adding elements, Removing elements, Traversing LinkedList
+        
+        ADD
+  * To add we need to break the already existing link.
+  * We can add at the beginning, at the end, and in between the nodes.
+
+  `At the Beginning LL` 
+  i.   Create a new node. (with some data, and it points to NULL)
+  ii.  Point it to the first node i.e add reference of the first node.
+       (Initially the reference is NULL) 
+  iii. Point the HEAD to the new node.
+
+  `At the End of LL`
+  i.   Create a new node. (with some data, and it points to NULL)
+  11.  Move to the last node.
+  iii. Change the link from NULL to the reference(address) of the new node.
+
+  `In between Nodes`
+  i.   Create a new node. (with some data, and it points to NULL)
+  ii.  Move to the node after which you want to add the new node.
+  iii. Change it's reference to the address of the new node.
+  iv.  Now, Change the reference of new node from NULL to the reference of the node which will be next to it.
+       
+        DELETE
+  `FIRST`
+  Change the HEAD's Link to the second node.
+
+  `LAST`
+  Go to the second last element, and make it's reference(pointer/link) as NULL
+
+  `In BETWEEN`
+   - Go to the element before the element to be deleted.
+   - Change it's link to the reference of the element which is next to the element that is to be deleted.
+
+   `TRAVERSE`
+   - Check if LinkedList is empty.
+   - If not, go through each node 
+"""
+
+
+class Node:
+    # Create individual nodes
+    def __init__(self, data):
+        self.data = data
+        self.ref = None  # We are creating independent nodes, hence the reference is None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_LL(self):
+        if self.head is None:
+            print("Linked List is empty!")
+        else:
+            n = self.head
+            while n is not None: # Until we've reached the last element
+                print(n.data)
+                n = n.ref # reference changes to the next node
+
+
+LL1 = LinkedList()
+LL1.print_LL()
 
